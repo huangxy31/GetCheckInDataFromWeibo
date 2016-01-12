0f28ba4ea4f7f125750adfca2989a5e85486ba3d
huangxy31 <hxy31@foxmail.com>
2016-01-12 13:12:44 +0800

    重构错误处理模块（try..except）

--- poi_timeline方法/getCheckInData.py
+++ poi_timeline方法/getCheckInData.py
@@ -7,7 +7,10 @@ import sys
 sys.path.append("..")
 import libs.myWeibo
 import math
-from libs.weibo import APIError 
+from libs.weibo import APIError
+import traceback
+from ssl import SSLError
+import time
 
 
 
@@ -25,9 +28,11 @@ def get_list_index_by_id(poiid):
         if poiid == poi:
             return i
     return -1
+
+
 #############################################################
 #微博部分
-#index:11646 page:1
+#index:21378 page:1
 #############################################################
 acess_token_index = 1
 app_info_index = 0
@@ -41,6 +46,12 @@ while my_client == -1:
     acess_token_index = int(raw_input(u"acess_token_index:"))
     app_info_index = int(raw_input(u"app_info_index:"))
     my_client = libs.myWeibo.get_client(app_info_index, acess_token_index)
+
+#############################################################
+#获取当前系统时间
+#############################################################
+def get_current_time():
+    return time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time()))
 
 #############################################################
 #调用微博api，获取某一POI的签到信息
@@ -72,6 +83,52 @@ def write_check_in_data(r, check_in_file):
             check_in_file.write(text)
             cur_check_in_index += 1
 
+#记录错误日志
+def write_error_log(error):
+    f=open("txt/log.txt",'a')
+    f.write(get_current_time() + '\n') #记录时间
+    f.write(str(error) + '\n') #记录错误
+    f.flush()  
+    f.close()
+    print u'写入日志'
+    
+#处理API错误
+def api_error_handle(error, poi_index, poi_id, page):
+    if '10023' not in str(error):
+    #日志记录
+        write_error_log(error)
+        
+    #'10023'表示User requests out of rate limit!
+    #'23201'表示Backend Service Connect Timeout!
+    #'10006'表示access token无效
+    if '10023' in str(error) or '23201' in str(error) or '10006' in str(error):
+        if '10006' in str(error):
+            print u'Access Token无效'
+        print u"index:"+str(poi_index), u"page:"+str(page), u"API调用出错"
+        return page
+    #'23805'表示不存在这个POI!
+    elif '23805' in str(error):
+        #没有签到数据就返回
+        print u"POI:"+str(poi_index)+u", POIID:"+str(poi_id)+u"没有签到数据"
+        return -2
+    else:
+        print error
+        print u"POI:"+str(poi_index)+u", POIID:"+str(poi_id)+u"出错"
+        sys.exit()
+
+#处理SSL timed out错误
+def ssl_error_handle(error, poi_index, poi_id, page):
+    #time out处理
+    #日志记录
+    write_error_log(error)
+    if 'timed out' in str(error):
+        print u"index:"+str(poi_index), u"page:"+str(page), u"超时重试！"
+        return page
+    else:
+        print error
+        print u"POI:"+str(poi_index)+u", POIID:"+str(poi_id)+u"出错"
+        sys.exit()
+
            
 #从某页起，获取poi_id的签到信息
 def write_poi_info(poi_index, poi_id, start_page):
@@ -82,7 +139,9 @@ def write_poi_info(poi_index, poi_id, start_page):
         #print poi_id, count, "?"
         if "statuses" not in r:
             #没有签到数据就返回
-            print u"POI:"+str(poi_index)+u", POIID:"+str(poi_id)+u"没有签到数据"
+            msg = u"POI:"+str(poi_index)+u", POIID:"+str(poi_id)+u"没有签到数据"
+            print msg
+            write_error_log(msg)
             return -2
         else:
             #创建文件
@@ -111,43 +170,25 @@ def write_poi_info(poi_index, poi_id, start_page):
                     print u"POI:"+str(poi_index)+u", POIID:"+str(poi_id)+u" 第"+str(i)+u"页完成"
                 #处理错误情况
                 except APIError, e:
-                    #'10023'表示User requests out of rate limit!
-                    if '10023' or '23201' or '10006' in str(e):
-                        print u"index:"+str(poi_index), u"page:"+str(i), u"API调用出错"
-                        return i
-                    #'23805'表示不存在这个POI!
-                    elif '23805' in str(e):
-                        #没有签到数据就返回
-                        print u"POI:"+str(poi_index)+u", POIID:"+str(poi_id)+u"没有签到数据"
-                        return -2
-                    else:
-                        print e
-                        print u"POI:"+str(poi_index)+u", POIID:"+str(poi_id)+u"出错"
-                        sys.exit()
+                    return api_error_handle(e, poi_index, poi_id, i)                        
+                #time out处理
+                except SSLError, e:
+                    return ssl_error_handle(e, poi_index, poi_id, i)
+
                 
             print u"POI:"+str(poi_index)+u", POIID:"+str(poi_id)+u"完成"
             check_in_file.flush()
             check_in_file.close()
             return -1
+
   
     except APIError, e:
-        #'10023'表示User requests out of rate limit!
-        #'23201'表示Backend Service Connect Timeout!
-        #'10006'表示access token无效
-        if '10023' or '23201' or '10006' in str(e):
-            if '10006' in str(e):
-                print u'Access Token无效'
-            print u"index:"+str(poi_index), u"page:"+str(start_page), u"API调用出错"
-            return start_page
-        #'23805'表示不存在这个POI!
-        elif '23805' in str(e):
-            #没有签到数据就返回
-            print u"POI:"+str(poi_index)+u", POIID:"+str(poi_id)+u"没有签到数据"
-            return -2
-        else:
-            print e
-            print u"POI:"+str(poi_index)+u", POIID:"+str(poi_id)+u"出错"
-            sys.exit()
+        return api_error_handle(e, poi_index, poi_id, start_page)            
+    #time out处理
+    except SSLError, e:
+        return ssl_error_handle(e, poi_index, poi_id, start_page)
+            
+        
 
 
 #############################################################
@@ -210,6 +251,9 @@ new_index, new_page = new_start_run()
 
 def loop_run():
     global new_index, new_page, start_index, start_page, my_client
+    #循环完成
+    if new_index == -1 and new_page=-1:
+        sys.exit()
     if new_index != -1:
         for i in range(22):
             acess_token_index = i
